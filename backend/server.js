6bc5fa1944be08724e9dac847b18315c432cef5f
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2025-09-18 21:28:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app=require('./app')
 const connectdatabase = require('./config/database')
 const cors=require('cors')
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://mohammedsuhail364.github.io/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
